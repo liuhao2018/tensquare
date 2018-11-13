dee4522713723f93c2b4e394f64c5036c95f450b
liuhao <liuhao@singulato.com>
2018-11-13 16:41:42 +0800

    init repo

---
+++ tensquare_base/src/main/java/com/tensquare/tensquare_base/TensquareBaseApplication.java
@@ -0,0 +1,12 @@
+package com.tensquare.tensquare_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensquareBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TensquareBaseApplication.class, args);
+    }
+}
